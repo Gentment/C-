dd5083bc617653a1592adc636630b5e36f091c46
LiuZiQiao <1844861420@qq.com>
2018-11-11 21:40:41 +0800

    new file:   day11_8/day11_8.sln 	new file:   day11_8/day11_8/day11_8.vcxproj 	new file:   day11_8/day11_8/day11_8.vcxproj.filters 	new file:   day11_8/day11_8/test.c

---
+++ day11_8/day11_8/test.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+
+#define Mul(x,y) ++x*++y
+
+int main()
+{
+	int a = 3; int b = 4;
+
+
+	return 0;
+}
+
+//int fun(int a, int b)
+//{
+//	return a + b;
+//}
+//
+//int main()
+//{
+//	int x = 4;
+//	int y = 5;
+//
+//	printf("%d\n",fun((x,y,x+y),(x+y)));
+//}
+
+//int main()
+//{
+//	int a = 1;
+//	int b = 2;
+//	int c = 3;
+//
+//	int i = ++a || ++b || ++c;
+//	printf("%d %d %d %d \n",i,a,b,c);
+//}
