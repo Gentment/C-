eb5f15b26d3df69725174b7876dd386ccc20570b
LiuZiQiao <1844861420@qq.com>
2018-11-12 23:41:03 +0800

    modified:   day11_10/day11_10/day11_10.vcxproj 	modified:   day11_10/day11_10/day11_10.vcxproj.filters 	modified:   day11_10/day11_10/test.cpp

--- day11_10/day11_10/test.cpp
+++ day11_10/day11_10/test.cpp
@@ -1,8 +1,21 @@
 #include<iostream>
-
+#include<vector>
 using namespace std;
+class Test {
+public:
+	void Permutation(string str) {
+	
+		str.push_back('a');
+		str.push_back('b');
+		str.push_back('c');
+		cout << str[1] << endl;
+
+	}
+};
+
 
 int main()
 {
-	
+	Test t;
+	t.Permutation("abc");
 }
