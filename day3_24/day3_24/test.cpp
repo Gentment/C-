17088996e404a7fa5bc999ab278537c0b6f08ac7
LiuZiQiao <1844861420@qq.com>
2019-03-26 23:50:24 +0800

    test

---
+++ day3_24/day3_24/test.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+bool match(const char *pattern, const char *str)
+{
+	if (*pattern == '\0' && *str == '\0')
+		return true;
+	if (*pattern == '\0' || *str == '\0')
+		return false;
+	if (*pattern == '?') {
+		return match(pattern + 1, str + 1);
+	}
+	else if (*pattern == '*') {
+		return match(pattern + 1, str) || match(pattern + 1, str + 1) || match(pattern, str + 1);
+	}
+	else if (*pattern == *str) {
+		return match(pattern + 1, str + 1);
+	}
+	return false;
+}
+
+int main()
+{
+	string pattern, str;
+	while (cin >> pattern >> str) {
+		cout << match(pattern.c_str(), str.c_str()) << endl;
+	}
+}
